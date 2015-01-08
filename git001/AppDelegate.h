fcf0d9828890d14f879202b3d92b5ef5cd6f22fd
kt <kt@a2ndeiMac.local>
2015-01-08 15:51:30 +0800

    Initial Commit

---
+++ git001/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git001
+//
+//  Created by kt on 15/1/8.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git001/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  git001
+//
+//  Created by kt on 15/1/8.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
